74bd7cb30c49e0b95875007682842abd00807cbe
peiman <50410853+aminpour-peiman@users.noreply.github.com>
2019-05-29 09:00:15 -0700

    Add files via upload

---
+++ 1212.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main(){
+	int x,i,ave,num;
+
+	
+	while(i<7 ){
+		++i;
+		cin>>x;
+		
+		num+=x;
+		ave=num/7;
+	}
+	cout<<ave;
+	
+	
+}

eec9254c538c61aac7746ac1f7a2c8bbe73446c2
peiman <50410853+aminpour-peiman@users.noreply.github.com>
2019-05-29 07:32:34 -0700

    Add files via upload

---
+++ نمره.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main(){
+
+	int x;
+	string y;
+	cout<<"please enter point";
+	cin>>x;
+	if(x>=0 && x<50 ){
+	cout<<y<<"failed";}
+	else if(x>=50 && x<60){
+	cout<<y<<"d";}
+	else if(x>=60 && x<70 ){
+cout<<	y<<"E";}
+	else if (x>=70 && x<80){
+cout<<	y<<"F";}
+	else if (x=100){
+
+	cout<<"g";
+	}
+	}
